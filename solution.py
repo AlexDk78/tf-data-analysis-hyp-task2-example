f20ae2d0a92382681befc54de10c7dc4eaf51265
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-09 11:26:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@ import numpy as np
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    import numpy as np
-    from scipy.stats import kstest
-    alpha = 0.08
-    stat, p_value = kstest(x, y)
-    return p_value > alpha
+    from scipy.stats import ks_2samp
+    _, p_value = ks_2samp(x, y)
+    return p_value < 0.08
