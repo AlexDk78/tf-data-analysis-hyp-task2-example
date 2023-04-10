b44eb9373c9d28dbbfc46d0b97b9886ff49b038e
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-10 19:24:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from scipy.stats import ks_2samp
-    _, p_value = ks_2samp(x, y)
-    return p_value < 0.08
+    return stats.cramervonmises_2samp(x, y).pvalue < 0.08 
