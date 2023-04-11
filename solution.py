185bc6676c38ecca8da85547ac909f4ea4370421
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-11 19:28:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy import stats
+
+chat_id = 897113152
+
+def solution(x: np.array, y: np.array) -> bool:
+    res = stats.cramervonmises_2samp(x, y)
+    return res.pvalue < 0.08
 
 
-chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool:
-    return stats.ttest_ind(x, y)[1] < 0.08
